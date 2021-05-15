40374235b13d8e28427515f62049fecde92cbc28
fguajard <franciscogm31@gmail.com>
2021-05-15 03:00:36 -0400

    agregado footer y smooth scroll en js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$("a").on("click", function (e) {
+    
+    e.preventDefault();
+    
+    const href = $(this).attr("href");
+    
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+  });
