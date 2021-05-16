cf6f7942160f9be08923ea81963ab222171238f2
fguajard <franciscogm31@gmail.com>
2021-05-16 00:48:43 -0400

    agregado estilo y seccion quienes somos desaparece en pequeño

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
-$("a").on("click", function (e) {
-    
-    e.preventDefault();
-    
-    const href = $(this).attr("href");
-    
-    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+$(document).ready(function () {
+  $("a").click(function (event) {
+    event.preventDefault();
+
+    var gato = this.hash;
+    $("html").animate(
+      {
+        scrollTop: $(gato).offset().top,
+      },
+      800
+    );
   });
+});
