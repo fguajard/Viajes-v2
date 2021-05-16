42d4c47677d4c183e2e7bd0645de652308998d18
fguajard <franciscogm31@gmail.com>
2021-05-16 06:52:11 -0400

    agregadas funcionalidades javascript,tooltip, hipervinculos sociales, alerts, barrra de navegacion cambia de color al salir de header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+// Activar tooltips
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+//smooth scroll
 $(document).ready(function () {
   $("a").click(function (event) {
     event.preventDefault();
@@ -5,9 +11,42 @@ $(document).ready(function () {
     var gato = this.hash;
     $("html").animate(
       {
-        scrollTop: $(gato).offset().top,
+        scrollTop: $(gato).offset().top - 20,
       },
       800
     );
   });
 });
+
+//cambia color de los iconos de la seccion "quienes-somos" y la caja que los contiene
+$(".quienes-somos .info .icono-container i").on("dblclick",function(){
+  
+  $(".quienes-somos .info .icono-container").css("background","blue")
+});
+
+//alert en click "read more"
+
+$(".quienes-somos .info h5").click(function(){
+  alert("Link en construccion");
+});
+
+
+var top1 = $(".quienes-somos").offset().top;
+if(!top1){
+  var top1 = $(".destacados").offset().top;
+}
+
+$(document).scroll(function() {
+  
+  var scrollPos = $(document).scrollTop();
+  
+  
+  if (scrollPos >= top1-40) {
+    $("nav").css('background-color', '#5bc0de');    
+  }
+  else{
+    $("nav").css('background-color', 'transparent');
+  }
+
+  });
+
